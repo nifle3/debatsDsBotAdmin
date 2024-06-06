121d55bd568ae04c3ac36020c20363025f3bad07
nifle3 <kuper116rus.2005@gmail.com>
2024-06-06 23:37:13 +0300

    add: init func in config

--- src/config/init.py
+++ src/config/init.py
@@ -1,4 +1,16 @@
-from config import Config
+from config import Config, TgConfig, MongoConfig
+from os import getenv
 
-def init() -> :
-    None
+
+def init() -> Config:
+    atlas_uri = getenv('ATLAS_URI')
+    db_name = getenv('DB_NAME')
+    token_bot = getenv('TOKEN_BOT')
+    admins_str = getenv('ADMINS')
+
+    admins = admins_str.split(',')
+
+    tg_config = TgConfig(token_bot, admins)
+    mongo_config = MongoConfig(atlas_uri, db_name)
+
+    return Config(mongo_config, tg_config)
