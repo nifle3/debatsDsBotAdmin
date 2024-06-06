2b03f2794fdc8c8637c4b87247251e814b8abcfb
nifle3 <kuper116rus.2005@gmail.com>
2024-06-07 00:32:46 +0300

    add: doc string for class and pylintrc file

--- src/config/config.py
+++ src/config/config.py
@@ -4,16 +4,22 @@ from collections.abc import Iterable
 
 @dataclass
 class MongoConfig:
+    """Data class with config for mongodb access"""
+
     atlas_uri : str
     db_name : str
 
 @dataclass
 class TgConfig:
+    """data class with config for tg bot"""
+
     token : str
     admins : Iterable[str]
 
 @dataclass
 class Config:
+    """data class with app config"""
+
     environment : str
     mongo_config : MongoConfig
     tg_config : TgConfig
